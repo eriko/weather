91bc68419db401a7f74a68f1899103fa63bafcff
Erik Ordway <ordwaye@evergreen.edu>
2013-09-04 16:53:41 -0700

    remove extra model and ie shit

--- app/models/person.rb
+++
@@ -1,9 +0,0 @@
-class Person < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :cas_authenticatable
-  # attr_accessible :title, :body
-end
